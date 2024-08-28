945b15ae231a27081441ae348a14b0499b82456e
TommyGong <luziyi.lucy@gmail.com>
2024-08-28 12:46:37 +0800

    feat: debug

--- inc/debug.h
+++ inc/debug.h
@@ -1,16 +1,48 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+#include <time.h>
 #include <stdio.h>
 
 #define _info_(format, ...) \
-    printf("\33[1;34m [INFO] \33[0m" format "\n", ##__VA_ARGS__)
+    do { \
+        time_t rawtime; \
+        struct tm *timeinfo; \
+        char buffer[20]; \
+        time(&rawtime); \
+        rawtime += 8 * 3600;\
+        timeinfo = localtime(&rawtime); \
+        strftime(buffer, 20, "%Y-%m-%d %H:%M:%S", timeinfo); \
+        printf("\33[1;34m [INFO] [%s] \33[0m" format " \n", buffer, ##__VA_ARGS__); \
+    } while(0)
+
+
 
 #define _msg_(format, ...)\
-    printf("\33[1;32m [MSG] \33[0m" format "\n", ##__VA_ARGS__)
+    do { \
+        time_t rawtime; \
+        struct tm *timeinfo; \
+        char buffer[20]; \
+        time(&rawtime); \
+        rawtime += 8 * 3600;\
+        timeinfo = localtime(&rawtime); \
+        strftime(buffer, 20, "%Y-%m-%d %H:%M:%S", timeinfo); \
+        printf("\33[1;32m [MSG] [%s] \33[0m" format " \n", buffer, ##__VA_ARGS__); \
+    } while(0)
 
 #define _debug_(format,...)\
-    printf("\33[1;91m [DEBUG] \33[0m" format "\n",##__VA_ARGS__)
+    do { \
+        time_t rawtime; \
+        struct tm *timeinfo; \
+        char buffer[20]; \
+        time(&rawtime); \
+        rawtime += 8 * 3600;\
+        timeinfo = localtime(&rawtime); \
+        strftime(buffer, 20, "%Y-%m-%d %H:%M:%S", timeinfo); \
+        printf("\33[1;91m [DEBUG] [%s] \33[0m" format " \n", buffer, ##__VA_ARGS__); \
+    } while(0)
+
+
 
 #define STATE_TO_STRING(state) \
     ((state) == CLOSED ? "CLOSED" : \
